dd731fb4cfe3a9ea5719fa7b3b8f174b9c8ed5a6
njokujr <njokuvictory216@gmail.com>
2023-07-04 19:23:40 +0000

    task 9 & 10 correction

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,10 @@
 export default function updateUniqueItems(map) {
-  for (const [key, value] of map.entries()) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
     }
-  }
+  });
 }
